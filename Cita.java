56807497090fc3671fb54fb9f48b36ba88321786
OnicarN <ebaudani@gmail.com>
2025-05-26 13:49:42 +0200

    commmt

---
+++ Cita.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+class Cita {
+    private String paciente;
+    private LocalDate fecha;
+    private LocalTime hora;
+
+    public Cita(String paciente, LocalDate fecha, LocalTime hora) {
+        this.paciente = paciente;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+}
